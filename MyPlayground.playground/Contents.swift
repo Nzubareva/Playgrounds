3e33abccb983d2c8f40cef111953407404165309
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-03-25 20:24:30 +0300

    added lesson 1 Playgrounds

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var typedVar : String
+let ccc = 42
+
+var ar = ["22", "33", "44"]
+print(ar)
+
